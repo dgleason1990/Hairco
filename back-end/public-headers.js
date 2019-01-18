0521a1c855e13706b685415bec80984120dbf0ed
David Gleason <dgleason1990@gmail.com>
2019-01-18 16:19:37 -0500

    forgot to add mongodb_uri

--- back-end/public-headers.js
+++ back-end/public-headers.js
@@ -3,3 +3,4 @@ module.exports.geolocation = process.env.geolocation;
 module.exports.accountSid = process.env.accountSid;
 module.exports.authToken = process.env.authToken;
 module.exports.SECRET_KEY = process.env.SECRET_KEY;
+module.exports.MONGODB_URI = process.env.MONGODB_URI;
