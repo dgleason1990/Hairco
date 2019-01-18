4487e13d26395ed2ea880c42c4f5bfbf7ffe50a7
David Gleason <dgleason1990@gmail.com>
2019-01-18 15:16:56 -0500

    changed files/one last commit before formatting for deployment

--- back-end/index.js
+++ back-end/index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const { key, geolocation, accountSid, authToken, SECRET_KEY } = require('./headers');
+const { geolocation, accountSid, authToken, SECRET_KEY, MONGODB_URI } = require('./headers');
 const axios = require('axios');
 const twilio = require('twilio');
 const mongoose = require('mongoose');
@@ -10,26 +10,22 @@ const bcrypt = require('bcrypt');
 const Users = require('./mongoDB/models/Users');
 const fs = require('fs');
 const cors = require('cors');
-const authorization = require('./Authorization')
+const authorization = require('./Authorization');
 
-mongoose.connect('mongodb://localhost:27017/hairco');
+// mongoose.connect('mongodb://localhost:27017/hairco');
+mongoose.connect(MONGODB_URI)
+.then(()=>console.log('MongoDB connected!'))
+.catch(err=>console.log(err))
 
 const connection = mongoose.connection;
 connection.on('open', ()=>{
     console.log('mongoose connected!')
 })
 
-// app.use((req, res, next) => {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-//     res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-//     next();
-// });
-
 app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('public', {
+app.use(express.static(__dirname + '/front-end/build', {
     setHeaders: function(res, path) { res.set("Cache-Control", "no-cache"); }
 }));
 
@@ -84,6 +80,7 @@ app.post('/appointment', async (req,res)=>{
 
 // Account registration
 app.post('/registration', async (req, res) => {
+    console.log(req.body)
     let address = req.body.address;
     let result = await axios.post('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + geolocation);
     let location = result.data.results[0].geometry.location;
@@ -150,83 +147,3 @@ app.post('/goals', authorization, (req,res)=>{
 })
 
 app.listen(process.env.PORT || 8080)
-// app.listen(8080, () => {
-//     console.log('You are connected to port 8080')
-//    })
-
-   
-
-// Tokens
-//    bcrypt.compare(password, user.password, (err, result) => {
-//     if(result){
-//       // passwords match! GENERATE TOKEN AND SEND BACK!
-//       const token = jwt.sign({subject: email}, SECRET_KEY);
-//       res.json({token: token, login:true})
-//     } 
-//     else{
-//       // passwords don't match! Danger! reject with a 401
-//       res.status(401).json({'msg': "invalid credentials", login:false});
-//     }
-// })
-
-
-// Authentication
-// function authorize(req, res, next) {
-//     // retrieve token from header with name 'authorization'
-//     const { authorization } = req.headers;
-    
-//     const token = authorization.split('Bearer ')[1];
-//     // if no toek, reject with 401 status
-//     if(!token) {
-//         return res.status(401).json({'msg': 'no token provided'});
-//     }
-//     // if there is a token, try to verifty it
-//     const decoded = jwt.verify(token, SECRET_KEY);
-//     // if it is NOT aythentic reject with 401
-//     if(!decoded) {
-//         return res.status(401).json({'msg': 'invalid token'});
-//     }
-//     // else if it IS authentic, store identtiy at req.user and call next
-//     req.user = decoded;
-//     //   calling next() signals we are done, call next request handler function
-//     next();
-// }
-
-
-// Front-end purposes {to grab token from local storage}
-// const init = {
-//     method: "GET",
-//     headers: {
-//         // here grab token from localStorager 
-//         authorization: `Bearer ${localStorage.getItem('token')}`
-//     }
-// }
-
-// Old Account registration
-// app.post('/registration', async (req,res)=>{
-//     let registrationData = req.body;
-//     console.log(registrationData)
-//     let address = registrationData.location;
-//     let result = await axios.post('https://maps.googleapis.com/maps/api/geocode/json?address=' + address + '&key=' + geolocation);
-//     console.log(result);
-//     let location = result.data.results[0].geometry.location;
-//     let latitude = location.lat;
-//     let longitude = location.lng;
-//     const user = new Users({
-//         name: registrationData.name,
-//         type:'Stylists',
-//         location:{
-//             type: 'Point',
-//             coordinates: [ longitude , latitude ]
-//         },
-//         phone_number: registrationData.phoneNumber,
-//         img: registrationData.img,
-//         rating: registrationData.rating
-//     });
-//     user.save()
-//     .then(saveduser=>{
-//         console.log(saveduser)
-//     })
-//     .catch(err=>console.log(err))
-//     res.sendStatus("successful registration")
-// })
---
+++ back-end/public-headers.js
@@ -0,0 +1,5 @@
+module.exports.googlemaps = process.env.googlemaps;
+module.exports.geolocation = process.env.geolocation;
+module.exports.accountSid = process.env.accountSid;
+module.exports.authToken = process.env.authToken;
+module.exports.SECRET_KEY = process.env.SECRET_KEY;
